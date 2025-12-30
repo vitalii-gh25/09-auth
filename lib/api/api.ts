553c33cce7cf0253e480e21b9e2c5f53ad5874b4
Vitalii_gh <a.g.vitalii2022@gmail.com>
2025-12-30 13:28:35 +0200

    Disable axios baseURL and credentials settings
    Comment out axios instance configuration.

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -14,6 +14,6 @@ withCredentials: true,
 // import axios from 'axios';
 
 // export const nextServer = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  withCredentials: true, // дозволяє axios працювати з cookie
-});
+// baseURL: 'http://localhost:3000/api',
+// withCredentials: true, // дозволяє axios працювати з cookie
+// });
