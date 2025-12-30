9aa5368b6dd9668f2a91c8c95938571bfa927a83
Vitalii_gh <a.g.vitalii2022@gmail.com>
2025-12-30 13:18:03 +0200

    Refactor API setup and clean up comments
    Updated API configuration to use environment variable for base URL and removed commented-out code.

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,70 +1,19 @@
-// // lib/api.ts
-// import axios from 'axios';
-// import type { Note } from '@/types/note';
+import axios from 'axios';
 
-// const API_BASE = 'https://notehub-public.goit.study/api';
-// const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
-// // Глобальна база
-// axios.defaults.baseURL = API_BASE;
+export const nextServer = axios.create({
+baseURL: BASE_URL,  
+withCredentials: true,
+});
 
-// // Глобальний заголовок з токеном (якщо є)
-// if (TOKEN) {
-//   axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`;
-// }
 
-// export interface FetchNotesResponse {
-//   notes: Note[];
-//   totalPages: number;
-// }
-
-// export interface FetchNotesParams {
-//   page?: number;
-//   perPage?: number;
-//   search?: string;
-//   tag?: string;
-//   sortBy?: 'created' | 'updated';
-//   signal?: AbortSignal;
-// }
-
-// export const fetchNotes = async ({
-//   page = 1,
-//   perPage = 12,
-//   search = '',
-//   tag,
-//   sortBy,
-//   signal,
-// }: FetchNotesParams = {}): Promise<FetchNotesResponse> => {
-//   const { data } = await axios.get<FetchNotesResponse>('/notes', {
-//     params: { page, perPage, search, tag, sortBy },
-//     signal,
-//   });
-//   return data;
-// };
-
-// export const fetchNoteById = async (id: string): Promise<Note> => {
-//   const { data } = await axios.get<Note>(`/notes/${id}`);
-//   return data;
-// };
-
-// export const createNote = async (
-//   note: Omit<Note, 'id' | 'createdAt' | 'updatedAt'>,
-// ): Promise<Note> => {
-//   const { data } = await axios.post<Note>('/notes', note);
-//   console.log('API createNote response:', data);
-//   return data;
-// };
-
-// export const deleteNote = async (id: string): Promise<Note> => {
-//   const { data } = await axios.delete<Note>(`/notes/${id}`);
-//   return data;
-// };
 
 // lib/api/api.ts
 
-import axios from 'axios';
+// import axios from 'axios';
 
-export const nextServer = axios.create({
+// export const nextServer = axios.create({
   baseURL: 'http://localhost:3000/api',
   withCredentials: true, // дозволяє axios працювати з cookie
 });
