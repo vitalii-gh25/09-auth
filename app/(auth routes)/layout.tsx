426fdd2aaa85cbff64f4ebf1cf08cc03ec212763
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-01-01 19:12:19 +0200

    rename import

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -7,7 +7,7 @@ import AuthProvider from '@/components/AuthProvider/AuthProvider';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
-import './globals.css';
+import '@/app/globals.css';
 
 interface Props {
   children: ReactNode;
